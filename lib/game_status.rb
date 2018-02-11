fd6dc08b9faec9488c2105e7ca5777587c482a98
Rima Jean <rima.jean@gmail.com>
2018-02-11 22:08:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,9 +19,9 @@ WIN_COMBINATIONS = [
     board.all? {|i| i != " " || i != ""}
     winning_array = WIN_COMBINATIONS.detect do |win_array|
       if win_array.all? {|position| board[position] == "X" } == true
-        winning_array.to_s
+        print winning_array
       elsif win_array.all? {|position| board[position] == "O" } == true
-        winning_array.to_s
+        print winning_array
       end
     end
   end
